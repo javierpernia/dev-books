4472c5ecf88ac7d24a387dcde23fd1aa6579bd8f
Javier Pernía <javier.perniac@gmail.com>
2026-01-29 16:01:14 -0400

    se instaló astrojs/node para el server island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
+import node from '@astrojs/node';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: node({
+    mode: 'standalone'
+  }),
   output: 'server',
   vite: {
     plugins: [tailwindcss()]
